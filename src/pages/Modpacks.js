6184b40067dbb4a5e24aac944017ef432dfdd2fd
CubeToPlay <hwvxyeej@gmail.com>
2024-01-03 22:59:07 -0600

    Changed CNAME location

--- src/pages/Modpacks.js
+++ src/pages/Modpacks.js
@@ -5,7 +5,7 @@ export default function Modpacks() {
         <>
             <h1>Modpacks</h1>
             <p><a href="https://drive.google.com/uc?id=_ID_&export=download">Download</a></p>
-            <p><a href="https://www.cubetoplay.ga/secret">Secret</a></p>
+            <p><a href="cubetoplay.com/secret">Secret</a></p>
 
             <h2>Pixelmon Launcher</h2>
             <p><a href="https://drive.google.com/uc?export=download&id=1TFl1ixbq6pv7igltNOdkXZwMivnxzgxs" download>Download</a> Launcher</p>
