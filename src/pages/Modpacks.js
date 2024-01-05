521caf1d7416a089e3aca00a88486a2d6088601f
CubeToPlay <hwvxyeej@gmail.com>
2024-01-05 13:47:46 -0600

    Updated Roblox and Modpacks

--- src/pages/Modpacks.js
+++ src/pages/Modpacks.js
@@ -7,7 +7,10 @@ export default function Modpacks() {
             <p><a href="https://drive.google.com/uc?id=_ID_&export=download">Download</a></p>
             <p><a href="cubetoplay.com/secret">Secret</a></p>
 
-            <h2>Pixelmon Launcher</h2>
+            <h2>Pixelmon Launcher 1.16.5 - Release</h2>
+            <p><a href="https://drive.google.com/uc?export=download&id=10ECEzh8mtZj0_3fVazDz8UAjqWar21T2" download>Download</a> Launcher</p>
+
+            <h2>Pixelmon Launcher 1.20.2 - Alpha</h2>
             <p><a href="https://drive.google.com/uc?export=download&id=1TFl1ixbq6pv7igltNOdkXZwMivnxzgxs" download>Download</a> Launcher</p>
         </>
     )
--- src/pages/Roblox.js
+++ src/pages/Roblox.js
@@ -4,7 +4,22 @@ export default function Roblox() {
     return (
         <>
             <h1>Roblox</h1>
-            <p>This page is dedicated to the Roblox experiences.</p>
+            <p>This page is dedicated to the Roblox creations. <a href="https://www.roblox.com/users/100162825/profile#!/creations">Profile</a></p>
+
+            <h2>Cube Explore</h2>
+            <p><a href="https://www.roblox.com/games/6143538633/Cube-Explore">link</a></p>
+
+            <h2>Tronish</h2>
+            <p><a href="https://www.roblox.com/games/2338327212/Tronish-v0-2-0">link</a></p>
+
+            <h2>Room</h2>
+            <p><a href="https://www.roblox.com/games/8576172003/Room">link</a></p>
+
+            <h2>Destroy The Tower</h2>
+            <p><a href="https://www.roblox.com/games/1362786037/Destroy-The-Tower-v1-8-3-Free-Private-Servers">link</a></p>
+
+            <h2>Survive The Falling Meteors</h2>
+            <p><a href="https://www.roblox.com/games/2983480424/Survive-The-Falling-Meteors-v1-3-0">link</a></p>
         </>
     )
 }
