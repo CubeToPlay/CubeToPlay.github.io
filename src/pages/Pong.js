005fecd27656c968f4b246bb6ed6896ff88c30bd
CubeToPlay <hwvxyeej@gmail.com>
2024-01-06 22:13:45 -0600

    Pong Now Working

--- src/pages/Pong.js
+++ src/pages/Pong.js
@@ -1,9 +1,21 @@
+import React, { useRef, useEffect } from 'react';
+import Game from '../ping-pong/src/game/Game.ts';
+
 export default function Pong() {
     document.title = "Pong";
 
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+      const game = new Game(canvasRef.current, 60, 50, 15);
+  
+      setInterval(() => game.updatePhysics(), game.MILLISECONDS_PER_PHYS);
+      setInterval(() => game.updateGraphics(), game.MILLISECONDS_PER_FRAME);
+    })
+
     return (
-        <>
-            <canvas id="pong_canvas" width="750" height="500"></canvas>
-        </>
-    )
+      <>
+        <canvas ref={canvasRef} width={750} height={500}/>
+      </>
+    );
 }
