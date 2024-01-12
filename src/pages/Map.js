6b5cc241a57e9ae734d806e8b24f995ac8d70063
CubeToPlay <yaj00002@umn.edu>
2024-01-12 15:08:02 -0600

    Removed Gopher City Bus

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,11 @@
-import Gopher_Map from "../Gopher-City-Bus/src/App"
+// import Gopher_Map from "../Gopher-City-Bus/src/App"
 
 export default function Map() {
     document.title = "Gopher Bus";
     
     return (
         <>
-            <Gopher_Map />
+            {/* <Gopher_Map /> */}
         </>
     )
 }
